ae1e203108ac98b853c82e0a84e523c76b230b32
William <williamtdcastro@gmail.com>
2018-02-09 23:43:06 -0200

    Source Folder created

--- src/Projetos/Flask/hello.py
+++ src/Projetos/Flask/hello.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi"
+    return "ASCII Converter"
+
+@app.route("/will")
+def will():
+    return "Hello William"
 
 if __name__ == "__main__":
     app.run()
